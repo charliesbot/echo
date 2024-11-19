867884f4505d4daa4bd4d788b858b71e59e68246
Charlie L <charliesbot@gmail.com>
2024-11-18 21:18:05 -0800

    add mocks

--- types/twitter.ts
+++ types/twitter.ts
@@ -1,41 +1,37 @@
-export interface TwitterError {
-  code: number;
-  message: string;
-}
-
-export interface FlowResponse {
-  errors?: TwitterError[];
-  flow_token: string;
-  status: string;
-  subtasks: Array<{
-    subtask_id: string;
-    open_account?: {
-      oauth_token: string;
-      oauth_token_secret: string;
-    };
-  }>;
-}
-
-export interface LoginRequest {
-  username: string;
-  password: string;
-  confirmation?: string;
-}
-
-export interface TwitterAuthResponse {
-  success: boolean;
-  isAuthenticated: boolean;
-  error?: string;
-}
-
-export interface TwitterCookie {
-  name: string;
-  value: string;
-  options?: {
-    domain?: string;
-    expires?: Date;
-    httpOnly?: boolean;
-    path?: string;
-    secure?: boolean;
+export type TweetResponse = {
+  data: Tweet[];
+  includes: {
+    tweets: Tweet[];
+    media: Media[];
   };
-}
+  meta: Meta;
+};
+
+export type Tweet = {
+  id: string;
+  text: string;
+  created_at: string;
+  edit_history_tweet_ids?: string[];
+  attachments?: {
+    media_keys: string[];
+  };
+  referenced_tweets?: {
+    type: "replied_to" | "quoted";
+    id: string;
+  }[];
+};
+
+export type Media = {
+  type: "photo" | "video";
+  url?: string;
+  preview_image_url?: string;
+  media_key: string;
+  duration_ms?: number;
+};
+
+type Meta = {
+  result_count: number;
+  next_token?: string;
+  newest_id: string;
+  oldest_id: string;
+};
